528df8f4100eeb305a71990d5e758b996d34b5b5
PeduZX <107699339+PeduZX@users.noreply.github.com>
2024-11-07 11:23:13 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
-// let um = document.getElementById('quadrado1');
-// let dois = document.getElementById('quadrado2');
-// let tres = document.getElementById('quadrado3');
+// let um = document.getElementById('primeira');
+// let dois = document.getElementById('segunda');
+// let tres = document.getElementById('terceira');
 
 // um.addEventListener('click', function() {
-//     if (um.style.backgroundColor === 'brown') {
-//         um.style.backgroundColor = 'blue';
+//     if (um.style.backgroundColor === 'red') {
+//         um.style.backgroundColor = 'black';
 //     } else {
 //         um.style.backgroundColor = 'red';
 //     }
 // });
 
 // dois.addEventListener('click', function() {
-//     if (dois.style.backgroundColor === 'green') {
-//         dois.style.backgroundColor = 'yellow';
+//     if (dois.style.backgroundColor === 'grey') {
+//         dois.style.backgroundColor = 'pink';
 //     } else {
-//         dois.style.backgroundColor = 'black';
+//         dois.style.backgroundColor = 'grey';
 //     }
 // });
 
 // tres.addEventListener('click', function() {
-//     if (tres.style.backgroundColor === 'pink') {
-//         tres.style.backgroundColor = 'orange';
+//     if (tres.style.backgroundColor === 'green') {
+//         tres.style.backgroundColor = 'brown';
 //     } else {
-//         tres.style.backgroundColor = 'purple';
+//         tres.style.backgroundColor = 'green';
 //     }
 // });
+
 
 //EX 2
 
 // let botao = document.getElementById('botao');
 // let h3 = document.getElementById('h3');
-// let zerar = document.getElementById('zerar');
+// let reset = document.getElementById('reset');
 // let i = 1;
 
 // botao.addEventListener('click', function() {
 //     h3.innerText = `Você clicou no botão ${i++} vezes`;
 // });
 
-// zerar.addEventListener('click', function() {
+// reset.addEventListener('click', function() {
 //     i = 1;
 //     h3.innerText = `Você clicou no botão 0 vezes`;
 // });
@@ -53,7 +54,7 @@
 //         botao.innerText = 'Ocultar';
 //     } else {
 //         frase.style.display = 'none';
-//         botao.innerText = 'ver';
+//         botao.innerText = 'Mostrar';
 //     }
 // });
 
@@ -62,45 +63,67 @@
 
 //EX 4
 // let botao = document.getElementById('botao');
-// let fraseInicial = document.getElementById('fraseInicial');
+// let mensagem = document.getElementById('mensagem');
 
 //     botao.addEventListener('click', function() {
-//         let escreva = prompt("mude a frase");
-//         fraseInicial.innerText = escreva;
+//         let escreva = prompt("Digite uma nova mensagem:");
+//         mensagem.innerText = escreva;
 //     });
 
 
 //Ex 5
 //     document.addEventListener("keydown", (event) => {
-//         let pressione = document.getElementById("pressione");
-//         pressione.innerText = `Você clicou no: ${event.key}`;
+//         let mensagem = document.getElementById("mensagem");
+//         mensagem.innerText = `Você pressionou: ${event.key}`;
 // });
     
-// Ex 6
+//Ex 6
 let votar = document.getElementById('votar');
-let alan = document.getElementById('alan');
-let borre = document.getElementById('borre');
+let messi = document.getElementById('messi');
+let cr7 = document.getElementById('cr7');
+let vtosM = document.getElementById('vtosM')
+let vtosC = document.getElementById('vtosC')
+let contadorM = 0;
+let contadorC = 0;
 
-alan.style.display = 'none';
-borre.style.display = 'none';
+messi.style.display = 'none';
+cr7.style.display = 'none';
 
 votar.addEventListener('click', function() {
-    votar.style.display = 'none';
-    let voto = prompt("Qual seu voto (Alan patrick ou Borré)?");
-    if (voto === 'Alan patrick') {
-        prompt('Você tem certeza?');
-        alert('Parabens, voce votou no Alan patrick');
-        alan.style.display = 'block';
-        
-        
-    } else if (voto === 'Borré') {
-        prompt('Você tem certeza?');
-        alert('Parabens, voce votou no Borré');
-        borre.style.display = 'block';
+    let voto = prompt("Qual seu voto (Cr7 ou Messi)?").toLowerCase(); 
+    if (voto === 'cr7') {
+        messi.style.display = 'none';
+       let confirmar = prompt('Você tem certeza?').toLowerCase(); 
+       if (confirmar === 'sim') {
+           alert('Parabéns, você votou no CR7');
+           contadorC++
+           cr7.style.display = 'block';
+       } else {
+           votar.style.display = 'block';
+           votar.innerText = "Vote de novo";
+       }
+    } else if (voto === 'messi') {
+
+        cr7.style.display = 'none';
+       let confirmar2 = prompt('Você tem certeza?').toLowerCase();
+       if (confirmar2 === 'sim') {
+           alert('Parabéns, você votou no Messi');
+           contadorM++
+           messi.style.display = 'block';
+       } else {
+           votar.style.display = 'block';
+           votar.innerText = "Vote de novo";
+       }
     } else {
-        alert('Voto NULO')
+        alert('Voto NULO');
+        votar.style.display = 'block';
     }
+  
+
+    vtosM.innerText = `Essa é a quantidade de votos do MESSI : ${contadorM}`
+    vtosC.innerText = `Essa é a quantidade de votos do CRISTIANO RONALDO:  ${contadorC}`
 });
+
 
 
 // Desafio
@@ -110,10 +133,10 @@ votar.addEventListener('click', function() {
 // let arrayI = [];
 
 // adicionar.addEventListener('click', function() {
-// let add = prompt('adicionar um item na lista? (sim ou nao)');
+// let add = prompt('deseja adicionar item a lista?');
 
 //     if (add === 'sim') {
-//         let item = prompt('qual item ?');
+//         let item = prompt('qual item deseja adicionar?');
 //         arrayI.push(item);
 //         alert('item adicionado com sucesso');
 //         lista.innerText = 'Esses são os itens presentes na lista:' + arrayI;
